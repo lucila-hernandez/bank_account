27b56716a186836f2e12f42534dd7a2b49d5d8a1
lucila-hernandez <lucila.hernandezdelcarmen@students.dominican.edu>
2024-10-27 17:10:28 -0700

    added withdraw method

--- bank_account.py
+++ bank_account.py
@@ -8,10 +8,27 @@ class BankAccount:
         self.balance = balance
   
     """
-    Helps add money to balance then updates balance. It will return the amount deposited and show the new balance with the new deposit.
+    Helps add money to balance then updates balance. 
+    It will return the amount deposited and show the new balance with the new deposit.
     """
     def deposit(self, amount):
         new_balance = self.balance + amount
         self.balance = new_balance
         print(f"Amount deposited: ${amount} new balance: ${self.balance}")
+
+    """
+    Checks to see if there's anough money in the balance. 
+    If there is not enouph money there will be a overdraft fee of 10, this will be subtracted from the balance and an insufficient funds message will show.
+    If there is enouph money, it will subtract the amount from the balance and update the new balance. It will show amount withdrawn and new balance. 
+    """
+    def withdraw(self, amount):
+        if amount > self.balance:
+            self.balance = self.balance - 10
+            print("Insufficient funds. Overdraft fee charged")
+        else:
+            new_balance = self.balance - amount
+            self.balance = new_balance 
+            print(f"Amount withdraw: ${amount} new balance ${self.balance}")
+
+
 
