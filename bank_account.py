b2a7f89e9416b1d8fa3b64f0d3fcbd5b41137cc2
lucila-hernandez <lucila.hernandezdelcarmen@students.dominican.edu>
2024-10-30 15:11:12 -0700

    updated bank_account.py

--- bank_account.py
+++ bank_account.py
@@ -1,69 +1,88 @@
-"""
-Class name BankAccount that holds bank attributes and methods
-"""
+
 class BankAccount:
+    """Class name BankAccount that holds bank attributes and methods"""
+
     def __init__(self, full_name, account_number, balance=0):
         self.full_name = full_name
         self.account_number = account_number
         self.balance = balance
   
-    """
-    Helps add money to balance then updates balance. 
-    It will return the amount deposited and show the new balance with the new deposit.
-    """
     def deposit(self, amount):
-        new_balance = self.balance + amount
-        self.balance = new_balance
-        print(f"Amount deposited: ${amount:.2f} new balance: ${self.balance:.2f}")
+        """Helps add money to balance then updates balance. It will return the amount deposited and show the new balance with the new deposit."""
+        self.balance += amount 
+        print(f"Amount deposited: ${amount:.2f}, new balance: ${self.balance:.2f}")
 
-    """
-    Checks to see if there's anough money in the balance. 
-    If there is not enouph money there will be a overdraft fee of 10, this will be subtracted from the balance and an insufficient funds message will show.
-    If there is enouph money, it will subtract the amount from the balance and update the new balance. It will show amount withdrawn and new balance. 
-    """
+
     def withdraw(self, amount):
+        """Checks to see if there's enough money in the balance. If there is not enough money there will be an overdraft fee of 10, this will be subtracted from the balance and an insufficient funds message will show. If there is enough money, it will subtract the amount from the balance and update the new balance. It will show amount withdrawn and new balance."""
         if amount > self.balance:
-            self.balance = self.balance - 10 #overdraft fee
-            print("Insufficient funds. Overdraft fee charged")
+            self.balance -= 10 #overdraft fee
+            print("Insufficient funds.")
         else:
-            new_balance = self.balance - amount
-            self.balance = new_balance 
-            print(f"Amount withdraw: ${amount:.2f} new balance ${self.balance:.2f}")
+            self.balance -= amount  
+            print(f"Amount withdraw: ${amount:.2f}, new balance ${self.balance:.2f}")
 
-    """
-    Show a account balance with a friendly message. Then shows current balance of the account. 
-    """
     def get_balance(self):
-        balance = self.balance
-        print(f"Thank you for being our bank user. Your current account balance is ${balance:.2f}")
-        return balance
-    
-    """
-    Adding interest to the users balance.
-    """
+        """Shows the account balance with a friendly message. Then shows the current balance of the account."""
+        print(f"Thank you for being a valuable client. Your current account balance is ${self.balance:.2f}")
+        return self.balance
+
     def add_interest(self):
+        """Adding interest to the user's balance."""
         self.balance = self.balance + (self.balance * 0.00083)
         return self.balance
     
-    """
-    Shows users name, account number and balance. 
-    """
-    def print_statement(self):
-        print(f"{self.full_name}\nAccount No.: {self.account_number}\nBalance: ${self.balance:.2f}")
 
+class BankAccount:
+    """Class name BankAccount that holds bank attributes and methods"""
+
+    def __init__(self, full_name, account_number, balance=0):
+        self.full_name = full_name
+        self.account_number = account_number
+        self.balance = balance
+  
+    def deposit(self, amount):
+        """Helps add money to balance then updates balance. It will return the amount deposited and show the new balance with the new deposit."""
+        self.balance += amount 
+        print(f"Amount deposited: ${amount:,.2f}, new balance: ${self.balance:,.2f}")
+
+
+    def withdraw(self, amount):
+        """Checks to see if there's enough money in the balance. If there is not enough money there will be an overdraft fee of 10, this will be subtracted from the balance and an insufficient funds message will show. If there is enough money, it will subtract the amount from the balance and update the new balance. It will show amount withdrawn and new balance."""
+        if amount > self.balance:
+            self.balance -= 10 #overdraft fee
+            print("Insufficient funds.")
+        else:
+            self.balance -= amount  
+            print(f"Amount withdraw: ${amount:,.2f}, new balance ${self.balance:,.2f}")
+
+    def get_balance(self):
+        """Shows the account balance with a friendly message. Then shows the current balance of the account."""
+        print(f"Thank you for being a valuable client. Your current account balance is ${self.balance:,.2f}")
+        return self.balance
+
+    def add_interest(self):
+        """Adding interest to the user's balance."""
+        self.balance = self.balance + (self.balance * 0.00083)
+        return self.balance
+    
+    def print_statement(self):
+        """Shows user's name, account number, and balance."""
+        print(f"{self.full_name}\nAccount No.: {self.account_number}\nBalance: ${self.balance:.2f}")
 
 #different object instances
 lucila_account = BankAccount("Lucila Hernandez", 12345678)
 adi_account = BankAccount("Adi Armendariz", 10101010)
 marceline_account = BankAccount("Marceline Hernandez", 20202020)
 
-#depositing money, then adding interest and showing bank statement.
+#depositing money, then withdrawing, and showing bank statement
 adi_account.deposit(30000)
-adi_account.add_interest()
-adi_account.print_statement()
+adi_account.withdraw(33000)
+adi_account.get_balance()
+
 
 # example code #1
-# created new bank account account intance
+# created new bank account intance
 mitchell_account = BankAccount("Mitchell", "03141592")
 #deposited $400,000 into "Mitchell's" account
 mitchell_account.deposit(400000)
@@ -75,6 +94,7 @@ mitchell_account.add_interest()
 mitchell_account.withdraw(150)
 #printed statement
 mitchell_account.print_statement()
+
 
 #example code #2
 # created new bank account account intance
